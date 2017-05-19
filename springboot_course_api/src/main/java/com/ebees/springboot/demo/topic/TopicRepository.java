e6047b67080c9c86da8a0ed362d257434531a554
Rasanka Jay <rasanka.lk@gmail.com>
2017-05-20 11:15:21 +1200

    Moving to seperate repo

--- springboot_course_api/src/main/java/com/ebees/springboot/demo/topic/TopicRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ebees.springboot.demo.topic;
-
-import org.springframework.data.repository.CrudRepository;
-
-// CrudRepository interface accepts the Entity and the ID type
-public interface TopicRepository extends CrudRepository<Topic, String> {
-	
-}
